6dea70d60ab66d785804b7c64b9394a1bd5061ec
eduardolopes777 <eduardolopes4888@outlook.com>
2022-05-10 18:41:06 -0300

    Input refatored

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,17 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+import { IconBaseProps } from 'react-icons';
+
+
+interface InputProps {
+  name: string;
+  placeholoder?: string;
+  icon?: React.ComponentType<IconBaseProps>
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +57,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
+
